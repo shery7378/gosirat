a871dad7e8d67bc4532464b94f18e39250be0629
shery7378 <shehryar@quadtrum.com>
2026-07-15 21:33:47 +0500

    added image and also updted numbers in about us page

--- src/components/about/AboutStats.tsx
+++ src/components/about/AboutStats.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 export const AboutStats = () => {
  return (
-  <section className="self-stretch bg-[#176938] flex flex-col items-start py-10 px-[60px] text-left text-6xl text-white font-[Inter] mq450:px-4 mq450:box-border" data-aos="fade-in">
+  <section className="self-stretch bg-[#176938] flex flex-col items-center py-10 px-[60px] text-left text-6xl text-white font-[Inter] mq450:px-4 mq450:box-border" data-aos="fade-in">
    <div className="self-stretch flex items-center justify-between flex-wrap content-center gap-5">
     <div className="h-auto flex-1 flex flex-col items-start min-w-[150px] md:min-w-[250px]">
      <div className="self-stretch flex flex-col items-start">
       <h2 className="m-0 relative text-[length:inherit] tracking-[-1.5px] leading-[60px] font-semibold font-[inherit] mq450:text-5xl mq450:leading-[48px] mq450:text-4xl mq450:leading-9">
-       120+
+       10+
       </h2>
      </div>
      <div className="flex flex-col items-start pt-3 px-0 pb-0 text-sm w-full">
@@ -21,7 +21,7 @@ export const AboutStats = () => {
     <div className="h-auto flex-1 flex flex-col items-start min-w-[150px] md:min-w-[250px]">
      <div className="self-stretch flex flex-col items-start">
       <h2 className="m-0 relative text-[length:inherit] tracking-[-1.5px] leading-[60px] font-semibold font-[inherit] mq450:text-5xl mq450:leading-[48px] mq450:text-4xl mq450:leading-9">
-       1250
+       250
       </h2>
      </div>
      <div className="flex flex-col items-start pt-3 px-0 pb-0 text-sm w-full">
@@ -35,7 +35,7 @@ export const AboutStats = () => {
     <div className="h-auto flex-1 flex flex-col items-start min-w-[150px] md:min-w-[250px]">
      <div className="self-stretch flex flex-col items-start">
       <h2 className="m-0 relative text-[length:inherit] tracking-[-1.5px] leading-[60px] font-semibold font-[inherit] mq450:text-5xl mq450:leading-[48px] mq450:text-4xl mq450:leading-9">
-       15K
+       1K+
       </h2>
      </div>
      <div className="flex flex-col items-start pt-3 px-0 pb-0 text-sm w-full">
@@ -46,10 +46,10 @@ export const AboutStats = () => {
       </div>
      </div>
     </div>
-    <div className="h-auto flex-1 flex flex-col items-start min-w-[150px] md:min-w-[165px]">
+    <div className="h-auto flex-1 flex flex-col items-start min-w-[150px] md:min-w-[250px]">
      <div className="self-stretch flex flex-col items-start">
       <h2 className="m-0 relative text-[length:inherit] tracking-[-1.5px] leading-[60px] font-semibold font-[inherit] mq450:text-5xl mq450:leading-[48px] mq450:text-4xl mq450:leading-9">
-       1.2 M
+       10K+
       </h2>
      </div>
      <div className="self-stretch flex flex-col items-start pt-3 px-0 pb-0 text-sm w-full">
